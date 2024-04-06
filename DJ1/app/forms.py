140c67344e35b371bfa53112ca8bc927b81be75d
DeeVanko <190103237@stu.sdu.edu.kz>
2024-04-07 00:29:02 +0600

    error messages

--- DJ1/app/forms.py
+++ DJ1/app/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, get_user_model
+from django.core.exceptions import ValidationError
 from .models import CustomUser
+import re
 
 class UserRegistrationForm(UserCreationForm):
     USER_TYPE_CHOICES = [
@@ -13,9 +15,25 @@ class UserRegistrationForm(UserCreationForm):
 
     user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = "Your password must contain at least 8 characters, including one capital letter, one number, and one special character."
+
     class Meta:
         model = CustomUser
         fields = ['username', 'password1', 'password2', 'user_type']
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 8:
+            raise ValidationError('Password must be at least 8 characters long.')
+        if not re.search(r'[A-Z]', password1):
+            raise ValidationError('Password must contain at least one capital letter.')
+        if not re.search(r'\d', password1):
+            raise ValidationError('Password must contain at least one number.')
+        if not re.search(r'[!@#$%^&*()_+{}|:"<>?]', password1):
+            raise ValidationError('Password must contain at least one special character.')
+        return password1
 
 
 User = get_user_model()
